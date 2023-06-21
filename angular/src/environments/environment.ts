99fe8ccda0526eb76ae2f955d05d2b9f0daeca47
vinhlam1412 <vinhlam1412@gmail.com>
2023-06-21 11:51:02 +0700

    setup

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'TeduEcommerce',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44349/',
+    redirectUri: baseUrl,
+    clientId: 'TeduEcommerce_App',
+    responseType: 'code',
+    scope: 'offline_access TeduEcommerce',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44396',
+      rootNamespace: 'TeduEcommerce',
+    },
+  },
+} as Environment;
---
+++ aspnet-core/src/TeduEcommerce.DbMigrator/TeduEcommerceDbMigratorModule.cs
@@ -0,0 +1,15 @@
+﻿using TeduEcommerce.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace TeduEcommerce.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(TeduEcommerceEntityFrameworkCoreModule),
+    typeof(TeduEcommerceApplicationContractsModule)
+    )]
+public class TeduEcommerceDbMigratorModule : AbpModule
+{
+
+}
